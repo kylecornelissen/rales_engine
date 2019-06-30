6158cec3306f411bc3b44a1863155e41f227f5a6
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-06-30 16:19:56 -0600

    Add id attribute to customer serializer

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,6 +1,6 @@
 class CustomerSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :first_name, :last_name
+  attributes :id, :first_name, :last_name
   has_many :invoices
   has_many :invoice_items
 end
