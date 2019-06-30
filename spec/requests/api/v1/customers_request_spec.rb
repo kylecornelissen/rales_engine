5529de9a28284239d4d4d3c060b60010fe638a09
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-06-29 22:55:06 -0600

    Add customer controllers

---
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+describe "Customers API" do
+  it "sends a list of customers" do
+    create_list(:customer, 3)
+
+    get '/api/v1/customers'
+
+    expect(response).to be_successful
+
+    customers = JSON.parse(response.body)["data"]
+    expect(customers.count).to eq(3)
+  end
+
+  it "can get one customer by its id" do
+    id = create(:customer).id
+
+    get "/api/v1/customers/#{id}"
+
+    customer = JSON.parse(response.body)["data"]
+
+    expect(response).to be_successful
+    expect(customer["id"].to_i).to eq(id)
+  end
+
+  it "can get one customer by given parameter" do
+    m1 = create(:customer, first_name: "bob")
+    m2 = create(:customer, first_name: "sally")
+    m3 = create(:customer, first_name: "sally")
+
+    get "/api/v1/customers/find?first_name=bob"
+
+    customer = JSON.parse(response.body)["data"]
+
+    expect(response).to be_successful
+    expect(customer["attributes"]["first_name"]).to eq("bob")
+  end
+
+  it "can get all customers by given parameter" do
+    m1 = create(:customer, first_name: "bob")
+    m2 = create(:customer, first_name: "sally")
+    m3 = create(:customer, first_name: "sally")
+
+    get "/api/v1/customers/find_all?first_name=sally"
+
+    customers = JSON.parse(response.body)["data"]
+
+    expect(response).to be_successful
+    expect(customers.count).to eq(2)
+  end
+end
