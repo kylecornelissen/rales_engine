9dd6616f3b96899a2ecf9560b00a3be0d63b2e68
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-01 01:48:38 -0600

    Add slice_unit_price method to invoice_items and items find controllers

--- app/controllers/api/v1/invoice_items/find_controller.rb
+++ app/controllers/api/v1/invoice_items/find_controller.rb
@@ -1,15 +1,29 @@
 class Api::V1::InvoiceItems::FindController < ApplicationController
   def show
+    if params[:unit_price]
+      slice_unit_price
+    end
     render json: InvoiceItemSerializer.new(InvoiceItem.find_by(invoice_item_params))
   end
 
   def index
+    if params[:unit_price]
+      slice_unit_price
+    end
     render json: InvoiceItemSerializer.new(InvoiceItem.where(invoice_item_params))
   end
 
   private
 
   def invoice_item_params
-    params.permit(:id, :quantity, :unit_price, :created_at, :updated_at)
+    params.permit(:id, :quantity, :unit_price, :created_at, :updated_at, :invoice_id, :item_id)
+  end
+
+  def slice_unit_price
+    if params[:unit_price].include? "\""
+      params[:unit_price].slice! "\""
+      params[:unit_price].slice! "\""
+    end
+    params[:unit_price] = ((params[:unit_price].to_f)*100).to_i
   end
 end
--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,15 +1,29 @@
 class Api::V1::Items::FindController < ApplicationController
   def show
+    if params[:unit_price]
+      slice_unit_price
+    end
     render json: ItemSerializer.new(Item.find_by(item_params))
   end
 
   def index
+    if params[:unit_price]
+      slice_unit_price
+    end
     render json: ItemSerializer.new(Item.where(item_params))
   end
 
   private
 
   def item_params
-    params.permit(:id, :name, :description, :unit_price, :created_at, :updated_at)
+    params.permit(:id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id)
+  end
+
+  def slice_unit_price
+    if params[:unit_price].include? "\""
+      params[:unit_price].slice! "\""
+      params[:unit_price].slice! "\""
+    end
+    params[:unit_price] = ((params[:unit_price].to_f)*100).round(2).to_i
   end
 end
