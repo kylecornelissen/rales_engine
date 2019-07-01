f916712bd5e84dcb098df74c4a8f1850040060c5
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-06-30 23:24:45 -0600

    Add most_revenue class method to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
+
+  def self.most_revenue(quantity)
+    select('items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+    .joins(:invoice_items, :transactions)
+    .where('transactions.result' => 'success')
+    .group(:id)
+    .order('revenue DESC')
+    .limit(quantity)
+  end
 end
