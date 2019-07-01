0b7072a2dec98d55586fd5f64a5e1d64d2eb1a9d
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-01 01:47:36 -0600

    Add unit_price attribute iteration to invoice_item and item serializers

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -3,4 +3,8 @@ class InvoiceItemSerializer
   attributes :id, :quantity, :unit_price, :invoice_id, :item_id
   belongs_to :item
   belongs_to :invoice
+
+  attribute :unit_price do |object|
+    object.unit_price = ('%.2f' % (object.unit_price.to_f/100))
+  end
 end
