534dbdf627e56e4dacf040e795dffdfa2c379397
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-01 01:49:47 -0600

    Add invoice_id to available transaction_params

--- app/controllers/api/v1/transactions/find_controller.rb
+++ app/controllers/api/v1/transactions/find_controller.rb
@@ -10,6 +10,6 @@ class Api::V1::Transactions::FindController < ApplicationController
   private
 
   def transaction_params
-    params.permit(:id, :credit_card_number, :result, :created_at, :updated_at)
+    params.permit(:id, :credit_card_number, :result, :created_at, :updated_at, :invoice_id)
   end
 end
