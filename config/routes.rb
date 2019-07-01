658a37e8b77666486c00dbe942bcd27ee8597af5
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-06-30 23:45:59 -0600

    Add most_items#index route to items namespace

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
         get '/:id/invoice_items', to: 'invoice_items#index'
         get '/:id/merchant', to: 'merchant#show'
         get '/most_revenue', to: 'most_revenue#index'
+        get '/most_items', to: 'most_items#index'
       end
 
       namespace :invoices do
