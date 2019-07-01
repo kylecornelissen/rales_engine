bac5c67e7ecd576deaf7024c219e0bfccd61f853
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-06-30 19:20:11 -0600

    Add most_revenue#index, most_items#index routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
         get '/random', to: 'random#show'
         get '/:id/items', to: 'items#index'
         get '/:id/invoices', to: 'invoices#index'
+        get '/most_revenue', to: 'most_revenue#index'
+        get '/most_items', to: 'most_items#index'
       end
 
       namespace :customers do
